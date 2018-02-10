9602d181221160007c048221d5f4d6158b2808cd
luizfernando10 <33362822+luizfernando10@users.noreply.github.com>
2018-02-10 04:35:33 -0200

    Add files via upload

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ import Logo from '../assets/logo.png'
 const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><img src={Logo} alt="Logo Marvel" /></a>
+            <a class="navbar-brand" href="/"><img src={Logo} alt="Logo Marvel" /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
